d1faa44d6504987575b6cafd369d95623b24d48f
Dmitriy <dmitriykharitonov333@gmail.com>
2024-05-26 20:10:45 +0300

    Решил 2 задачу

--- Task2.py
+++ Task2.py
@@ -1,2 +1,16 @@
 # Задача 2. Отгадать 2 числа, зная сумму и произведение
 
+import os
+os.system('cls')
+
+numbers = [7, 12]
+print(numbers)
+
+for a in range(0, 1001):
+    for b in range(0, 1001):
+        if a + b == numbers[0] and a * b == numbers[1]:
+            set = {a, b}
+print(*set)
+        
+            
+
